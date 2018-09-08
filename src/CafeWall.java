dbf56f7d56e25fcd542a4b5730033436aa3a1351
Mi Kyung Han <mhan@calbaptist.edu>
2018-09-07 22:06:17 -0700

    Template for HW2 Initialization

---
+++ src/CafeWall.java
@@ -0,0 +1,16 @@
+/**
+ * Created by YOUR_NAME on DATE
+ * You shouldn't use any if-else statements
+ * Put comments in your code
+ * Follow freedom of information and gilligans island rule
+ * Discussed with: XXX
+ **/
+
+import java.awt.*;
+
+public class CafeWall {
+
+    public static void main(String[] args) {
+        //Remove this comment and implement your code
+    }
+}
