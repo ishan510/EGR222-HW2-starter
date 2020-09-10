96c23fdc2acc2f2ecf8053f01d3df7a463a26678
Prof Han <mhan@calbaptist.edu>
2020-09-10 07:38:41 -0700

    adding initializePanel method

--- src/CafeWall.java
+++ src/CafeWall.java
@@ -14,11 +14,31 @@ public class CafeWall {
     private static final Integer MORTAR = 2; //Your code should work for both cases when MORTAR is 1 or 2
 
     public static void main(String[] args) throws IOException {
-        DrawingPanel panel = new DrawingPanel(650, 420); //Change the pixel dimensions according to spec
+        DrawingPanel panel = initializePanel(100, 100, Color.ORANGE); //Change the parameters according to spec!
         //Write your code here
 
 
 
         panel.save("output" + MORTAR + ".png"); //Do not remove (this code must be the last statement in main)
+    }
+
+    //Do not remove or change this method
+    /**
+     * This method creates a new drawing panel object with given dimensions and background color
+     * and returns the panel object
+     * @param width the width (in pixels) of drawing panel object to create
+     * @param height the height (in pixels) of drawing panel object to create
+     * @param bgColor the background color of drawing panel object to create
+     * @return a new drawing panel object with the given width, height, and background color
+     */
+    private static DrawingPanel initializePanel(int width, int height, Color bgColor) {
+        //Create the drawing panel object
+        DrawingPanel panel = new DrawingPanel(width, height);
+
+        //Set background color
+        panel.setBackground(bgColor);
+
+        //Return the panel object
+        return panel;
     }
 }
